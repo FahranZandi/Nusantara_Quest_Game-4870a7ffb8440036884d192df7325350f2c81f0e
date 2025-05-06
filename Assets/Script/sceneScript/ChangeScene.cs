85c1718d6356d9a1b4395542974048cbeb2ed167
FahranZandi <162786907+FahranZandi@users.noreply.github.com>
2025-05-06 10:12:47 +0800

    update34

--- Assets/Script/sceneScript/ChangeScene.cs
+++ Assets/Script/sceneScript/ChangeScene.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement; // Import SceneManager untuk mengelola scene
 
-public class ChangeScene
+public class ChangeScene : MonoBehaviour
 {
-    
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // Ganti scene ke scene yang diinginkan
+            SceneManager.LoadScene("Scene3"); // Ganti "SceneName" dengan nama scene yang ingin dimuat
+        }
+    }
 }
+
+
